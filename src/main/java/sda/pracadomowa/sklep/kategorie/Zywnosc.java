b32bc770f70df33b50604388424a8205f9d5e387
RCyro <r.cyron95@gmail.com>
2022-12-03 18:29:41 +0100

    Update class

--- src/main/java/sda/pracadomowa/sklep/kategorie/Zywnosc.java
+++ src/main/java/sda/pracadomowa/sklep/kategorie/Zywnosc.java
@@ -1,4 +1,22 @@
 package sda.pracadomowa.sklep.kategorie;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Zywnosc {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
